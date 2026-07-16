ad918377315fe4611d0733cf6f71745f5282aa60
HibaYaqoob <16j2021906@utas.edu.om>
2026-07-17 02:45:20 +0400

    Fix the classes

--- Task8/Program.cs
+++ Task8/Program.cs
@@ -1,80 +1,82 @@
 ﻿namespace Task8
 {
-    internal class Program
+
+    // --------------------------------------------------------------
+    // Room Class:
+    // -------------------------------------------------------------
+    public class Room
     {
-        // --------------------------------------------------------------
-                                  // Room Class:
-        // --------------------------------------------------------------
-       
-        public class Room
+        public int roomNumber;
+        public string roomType;
+        public double pricePerNight;
+        public bool isAvailable;
+
+        // Constructor
+        public Room(int number, string type, double price, bool available)
         {
-            public int roomNumber;
-            public string roomType;
-            public double pricePerNight;
-            public bool isAvailable;
-
-            // Constructor
-            public Room(int number, string type, double price, bool available)
-            {
-                roomNumber = number;
-                roomType = type;
-                pricePerNight = price;
-                isAvailable = available;
-            }
-
-            // Method to display room details
-            public void displayRoom()
-            {
-                Console.WriteLine("Room Number: " + roomNumber);
-                Console.WriteLine("Room Type: " + roomType);
-                Console.WriteLine("Price Per Night: " + pricePerNight);
-                Console.WriteLine("Available: " + isAvailable);
-            }
+            roomNumber = number;
+            roomType = type;
+            pricePerNight = price;
+            isAvailable = available;
         }
 
-        // --------------------------------------------------------------
-        // Guest Class:
-        // --------------------------------------------------------------
-
-        public class Guest
+        // Method to display room details
+        public void displayRoom()
         {
-            public int guestId;
-            public string guestName;
-            public int roomNumber;
-            public string checkInDate;
-            public int totalNights;
-            public double pricePerNight;
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Room Type: " + roomType);
+            Console.WriteLine("Price Per Night: " + pricePerNight);
+            Console.WriteLine("Available: " + isAvailable);
+        }
+    }
+
+    // --------------------------------------------------------------
+    // Guest Class:
+    // --------------------------------------------------------------
+
+    public class Guest
+    {
+        public int guestId;
+        public string guestName;
+        public int roomNumber;
+        public string checkInDate;
+        public int totalNights;
+        public double pricePerNight;
 
 
-            // Constructor
-            public Guest(int id, string name, int room, string date, int nights, double price)
-            {
-                guestId = id;
-                guestName = name;
-                roomNumber = room;
-                checkInDate = date;
-                totalNights = nights;
-                pricePerNight = price;
-            }
+        // Constructor
+        public Guest(int id, string name, int room, string date, int nights, double price)
+        {
+            guestId = id;
+            guestName = name;
+            roomNumber = room;
+            checkInDate = date;
+            totalNights = nights;
+            pricePerNight = price;
+        }
 
 
-            // Method to display guest details
-            public void displayGuest()
-            {
-                Console.WriteLine("Guest ID: " + guestId);
-                Console.WriteLine("Guest Name: " + guestName);
-                Console.WriteLine("Room Number: " + roomNumber);
-                Console.WriteLine("Check In Date: " + checkInDate);
-                Console.WriteLine("Total Nights: " + totalNights);
-            }
+        // Method to display guest details
+        public void displayGuest()
+        {
+            Console.WriteLine("Guest ID: " + guestId);
+            Console.WriteLine("Guest Name: " + guestName);
+            Console.WriteLine("Room Number: " + roomNumber);
+            Console.WriteLine("Check In Date: " + checkInDate);
+            Console.WriteLine("Total Nights: " + totalNights);
 
 
             // Method to calculate total cost
             public double calculateTotalCost()
-            {
-                return totalNights * pricePerNight;
-            }
+        {
+            return totalNights * pricePerNight;
         }
+
+        internal class Program
+    {-
+       
+        
+        
 
 
 
